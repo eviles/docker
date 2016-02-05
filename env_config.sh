525ec70b161266ec0ee024c6122bc3197810b53b
eviles <eviles.tw@gmail.com>
2016-02-05 14:49:20 +0800

    Update env_config.sh

--- env_config.sh
+++ env_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i 's/DocumentRoot "\/var\/www\/html"/DocumentRoot "$2"/g' /etc/httpd/conf/httpd.conf
-sed -i 's/<Directory "\/var\/www">/<Directory "$1">/g' /etc/httpd/conf/httpd.conf
-sed -i 's/appBase="webapps"/appBase="$4"/g' /usr/local/tomcat/conf/server.xml
+sed -i 's/DocumentRoot "\/var\/www\/html"/DocumentRoot "$HTTPD_ROOT"/g' /etc/httpd/conf/httpd.conf
+sed -i 's/<Directory "\/var\/www">/<Directory "$HTTPD_BASE">/g' /etc/httpd/conf/httpd.conf
+sed -i 's/appBase="webapps"/appBase="$TOMCAT_BASE"/g' /usr/local/tomcat/conf/server.xml
 
-mkdir -p $1
-mkdir -p $2
-mkdir -p $4
-ln -s $HTTPD_BASE $3
+mkdir -p $HTTPD_BASE
+mkdir -p $HTTPD_ROOT
+mkdir -p $TOMCAT_BASE
+ln -s $HTTPD_BASE $HTTPD_LINK
