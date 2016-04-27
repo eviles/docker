50e509a33bed95bea00003141ea4fa54b30f65e9
eviles <eviles.tw@gmail.com>
2016-04-27 15:27:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --user=mysql--datadir=/var/lib/mysql
+fi
+set -- mysqld "$@"
+exec "$@" --wsrep_node_address="${HOSTNAME}" \
+    --wsrep_cluster_name="${CLUSTER_NAME}" \
+    --wsrep_cluster_address="gcomm://${CLUSTER}" \
+    --wsrep_node_name="${HOSTNAME}"
