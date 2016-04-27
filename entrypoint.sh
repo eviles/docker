218629a4b9e324e759fb7e0243ff950210259fa7
eviles <eviles.tw@gmail.com>
2016-04-27 15:59:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql--datadir=/var/lib/mysql
 fi
 set -- mysqld "$@"
-exec "$@" --wsrep_node_address="${HOSTNAME}" \
+if [ ${CLUSTER} = "master" ]; then
+  exec "$@" --wsrep_node_address="${HOSTNAME}" \
+    --wsrep_cluster_name="${CLUSTER_NAME}" \
+    --wsrep_new_cluster --wsrep_cluster_address="gcomm://" \
+    --wsrep_node_name="${HOSTNAME}"
+else
+  exec "$@" --wsrep_node_address="${HOSTNAME}" \
     --wsrep_cluster_name="${CLUSTER_NAME}" \
     --wsrep_cluster_address="gcomm://${CLUSTER}" \
     --wsrep_node_name="${HOSTNAME}"
+fi
