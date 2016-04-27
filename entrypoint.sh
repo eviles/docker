f54f37a915aea89bfd6fdcb83d26ae38b4356863
eviles <eviles.tw@gmail.com>
2016-04-27 18:03:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql--datadir=/var/lib/mysql
+	mysql_install_db --user=mysql--datadir=/var/lib/mysql --rpm
 fi
 set -- mysqld "$@"
 if [ ${CLUSTER} = "master" ]; then
