e2faf243b18fbea9232ade1c41e27ee283fe1c2f
eviles <eviles.tw@gmail.com>
2016-04-27 18:42:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,40 @@
 
 set -x
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql--datadir=/var/lib/mysql --rpm
-fi
+export TERM=xterm
+
 set -- mysqld "$@"
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  mysql_install_db --user=mysql--datadir=/var/lib/mysql --rpm
+
+  "$@" --skip-networking &
+  pid="$!"
+
+  mysql=( mysql --protocol=socket -uroot )
+
+  for i in {30..0}; do
+    if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+      break
+    fi
+    echo 'MySQL init process in progress...'
+    sleep 1
+  done
+  if [ "$i" = 0 ]; then
+    echo >&2 'MySQL init process failed.'
+    exit 1
+  fi
+
+  echo "CREATE USER 'sstuser'@'%' IDENTIFIED BY 'sstpass' ;" | "${mysql[@]}"
+  echo "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sstuser'@'%' ;" | "${mysql[@]}"
+  echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+  
+  if ! kill -s TERM "$pid" || ! wait "$pid"; then
+    echo >&2 'MySQL init process failed.'
+    exit 1
+  fi
+fi
+
 if [ ${CLUSTER} = "master" ]; then
   exec "$@" --wsrep_node_address="${HOSTNAME}" \
     --wsrep_cluster_name="${CLUSTER_NAME}" \
