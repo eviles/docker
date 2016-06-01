75ff29e6bd4c6ffdb366fed389594fc20c50efdf
eviles <eviles.tw@gmail.com>
2016-06-01 23:25:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ export TERM=xterm
 set -- mysqld "$@"
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-  mysql_install_db --user=mysql--datadir=/var/lib/mysql --rpm
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
 
   "$@" --skip-networking &
   pid="$!"
