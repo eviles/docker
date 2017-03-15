b70f3085de232ee9f39c6c799a485a08658a024b
eviles <eviles.tw@gmail.com>
2017-03-15 12:50:33 +0800

    Update run.sh

--- centos-sshd/run.sh
+++ centos-sshd/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$ROOT_PWD" == "" ]; then
-  ROOT_PWD=123456
+  ROOT_PWD=`date +%s | sha256sum | base64 | head -c 32 ; echo`
+  echo "SETTING ROOT PASSWORD:$ROOT_PWD"
 fi
 echo "root:$ROOT_PWD" | chpasswd
 
