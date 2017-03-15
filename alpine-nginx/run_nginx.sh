28be69706c352138d1953856f32c461488eb9b3d
eviles <eviles.tw@gmail.com>
2017-03-15 15:07:20 +0800

    Update run_nginx.sh

--- alpine-nginx/run_nginx.sh
+++ alpine-nginx/run_nginx.sh
@@ -5,7 +5,7 @@ if [ ! -d "/run/nginx" ]; then
   mkdir /run/nginx
 fi
 
-#DOMAINS => example.com,www.example.com
+# DOMAINS => example.com,www.example.com
 if [ "$DOMAINS" != "" ]; then
   IFS=',' read -r -a ADDR <<< "$DOMAINS"
   if [ ! -d "/etc/letsencrypt/live/${ADDR[0]}" ]; then
@@ -13,6 +13,10 @@ if [ "$DOMAINS" != "" ]; then
   fi
 else
   certbot renew
+fi
+
+if [ -f "/etc/ssl/certs/dhparam.pem" ]; then
+  openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 fi
 
 usr/sbin/nginx -g "daemon off;"
