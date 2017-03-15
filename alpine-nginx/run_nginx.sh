8b8a4f100913a63b5f7345d63f20957ad399d7ca
eviles <eviles.tw@gmail.com>
2017-03-15 15:49:26 +0800

    Update run_nginx.sh

--- alpine-nginx/run_nginx.sh
+++ alpine-nginx/run_nginx.sh
@@ -9,14 +9,17 @@ fi
 if [ "$DOMAINS" != "" ]; then
   IFS=',' read -r -a ADDR <<< "$DOMAINS"
   if [ ! -d "/etc/letsencrypt/live/${ADDR[0]}" ]; then
-    # Replace default.conf
-    sed 's/server_name example.com/server_name ${DOMAINS}/g' /etc/nginx/conf.d/default.conf.new | sed 's/example.com/${ADDR[0]}/g' > /etc/nginx/conf.d/default.conf
-    nginx
-    certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --register-unsafely-without-email --webroot-path /var/lib/nginx/html -domains "${DOMAINS}"
-  fi
-  # Generate Strong Diffie-Hellman Group
-  if [ -f "/etc/ssl/certs/dhparam.pem" ]; then
+    # Generate Strong Diffie-Hellman Group
     openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+    # Replace default.conf
+    SERVER_NAME=`echo $DOMAINS | sed 's/,/ /g'`
+    sed 's/server_name example.com/server_name ${SERVER_NAME}/g' /etc/nginx/conf.d/default.conf.new | sed 's/example.com/${ADDR[0]}/g' > /etc/nginx/conf.d/default.conf
+    # Start NGINX for Certbot
+    nginx
+    # Obtain a Certificate
+    certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --register-unsafely-without-email --webroot-path /var/lib/nginx/html -domains "${DOMAINS}"
+    # Stop NGINX
+    nginx -s quit
   fi
 else
   certbot renew
