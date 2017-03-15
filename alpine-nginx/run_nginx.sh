a0a29a10cf905336adb658522c5f77b380098138
eviles <eviles.tw@gmail.com>
2017-03-15 15:19:10 +0800

    Update run_nginx.sh

--- alpine-nginx/run_nginx.sh
+++ alpine-nginx/run_nginx.sh
@@ -9,14 +9,19 @@ fi
 if [ "$DOMAINS" != "" ]; then
   IFS=',' read -r -a ADDR <<< "$DOMAINS"
   if [ ! -d "/etc/letsencrypt/live/${ADDR[0]}" ]; then
+    # Replace default.conf
+    cp /etc/nginx/conf.d/default.conf.new /etc/nginx/conf.d/default.conf
+    nginx
     certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --register-unsafely-without-email --webroot-path /var/lib/nginx/html -domains "${DOMAINS}"
+    
+    
+  fi
+  # Generate Strong Diffie-Hellman Group
+  if [ -f "/etc/ssl/certs/dhparam.pem" ]; then
+    openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
   fi
 else
   certbot renew
 fi
 
-if [ -f "/etc/ssl/certs/dhparam.pem" ]; then
-  openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-fi
-
-usr/sbin/nginx -g "daemon off;"
+/usr/sbin/nginx -g "daemon off;"
